9bccd42a0706b18a6c6dd9902d92d9e1ca2e0b63
Yeye-7812 <sibongilesimbeyejy@gmail.com>
2022-11-29 07:59:22 -0800

    Three colors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    digit = abs(number) % 10
-    print("{:d}".format(digit), end='')
-    return
+    tmp = int(repr(number)[-1])
+    print("{}".format(tmp), end="")
+    return tmp
